82614ae70396411d71063edad6b01a450a310e17
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-09-24 12:29:26 +0100

    Add files via upload

---
+++ load.sh
@@ -0,0 +1,15 @@
+echo -n [+] finding Followers...
+	tput sc
+	n=0;
+	while true; do
+		if [ $n -lt 100 ]; then
+			let n++;
+			sleep 0.01
+			tput rc
+			tput ed
+	
+			echo -n $n;
+		else exit 0;
+		fi
+	done
+echo
